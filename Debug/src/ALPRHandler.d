6947f22789f30fdc8f2612910a62e9e559d09582
MW <mateuszwag@gmail.com>
2015-06-07 11:54:58 +0200

    Cleanup.

--- Debug/src/ALPRHandler.d
+++
@@ -1,6 +0,0 @@
-src/ALPRHandler.d: ../src/ALPRHandler.cpp ../src/include/ALPRHandler.h \
- /home/mateusz/workspace/pgjs/openalpr/src/openalpr/alpr.h
-
-../src/include/ALPRHandler.h:
-
-/home/mateusz/workspace/pgjs/openalpr/src/openalpr/alpr.h:
--- Debug/src/QueueHandler.d
+++
@@ -1,6 +0,0 @@
-src/QueueHandler.d: ../src/QueueHandler.cpp ../src/include/QueueHandler.h \
- ../src/include/MessageHandler.h
-
-../src/include/QueueHandler.h:
-
-../src/include/MessageHandler.h:
